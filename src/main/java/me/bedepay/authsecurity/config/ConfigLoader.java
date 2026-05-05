d7666f4eda6cda2a888950e4bf05b840afd4ba2b
BeDePaY <dima.sergeev19@yandex.by>
2026-05-05 23:10:32 +0300

    Bump config version for captcha cleanup

--- src/main/java/me/bedepay/authsecurity/config/ConfigLoader.java
+++ src/main/java/me/bedepay/authsecurity/config/ConfigLoader.java
@@ -20,7 +20,7 @@ import java.util.List;
  */
 public final class ConfigLoader {
 
-    private static final int CONFIG_VERSION = 2;
+    private static final int CONFIG_VERSION = 3;
 
     private ConfigLoader() {}
 
